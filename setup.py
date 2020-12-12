ff7317b92a073e960a2ba21cf593e7f3976db20d
duracel1540 <64642640+duracel1540@users.noreply.github.com>
2020-12-12 21:16:02 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version="1.0.0",
     author='Byeongseo Yu',
     author_email="qudtj1540@gmail.com",
-    url="https://github.com/skku-capstone-2020-fall-group13/Backend.git",
+    url="https://github.com/skku-capstone-2020-fall-group13/logic.git",
     install_requires= [
         'pillow>=2.0.0'
     ],
