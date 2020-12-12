45e3c00155c5d8cb0e768d78a393ff260394ca8a
duracel1540 <64642640+duracel1540@users.noreply.github.com>
2020-12-12 21:43:58 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="logic",
+    name="comflogic",
     version="1.0.0",
     author='Byeongseo Yu',
     author_email="qudtj1540@gmail.com",
