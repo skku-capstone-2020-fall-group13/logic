51be079f8227acafcce62347d2c0e583c56fd8a5
Junhyun Kim <me@junbread.win>
2020-12-15 11:52:38 +0900

    코드 정리

--- comflogic/analyze.py
+++ comflogic/analyze.py
@@ -8,17 +8,13 @@ def analyze(category_arr):
   response['ratio'] = []
   response['score'] = []
 
-  hcount = 0
-  acount = 0
-  field = 0.0
-
   color_classes = ["도로", "주택", "아파트", "공장", "강", "논밭", "녹지", "대형건물", "미분류"]
 
   colors, counts = np.unique(category_arr, return_counts=1)
   for idx, x in enumerate(colors):
     counts[idx] = 0
 
-  # 10% ~ 90% 사이의 값은 중앙 그 외는 외곽으로 판정
+  # 10% ~ 90% 사이의 값은 중앙, 그 외는 외곽으로 판정
   hmin = arr_height * 0.1
   hmax = arr_height * 0.9
   wmin = arr_width * 0.1
@@ -33,56 +29,64 @@ def analyze(category_arr):
         i = np.where((colors == category_arr[h][w]))
         counts[i[0][0]] += 0.7
 
-  env_plus_ret = 0.0
-  env_minus_ret = 0.0
-  conv_ret = 0.0
+  green_score = 0.0
+  green_synergy = 0
 
-  syn = 0
+  factory_road_score = 0.0
+  convenience_score = 0.0
+
+  house_score = 0.0
+  house_count = 0
+  apartment_count = 0
+
+  development_score = 100
 
   for index, color in enumerate(colors):
     count = counts[index]
     
-    proportion = (100 * count) / (sum(counts)) #외곽 픽셀 0.7 반영
+    proportion = (100 * count) / (sum(counts)) # 외곽 픽셀 0.7 반영
     ci = int(color)
 
-    response['ratio'].append({"class" : color_classes[ci] , "proportion" : proportion})
+    response['classes'].append({"name" : color_classes[ci] , "proportion" : proportion})
     
-    if(color == 6): #녹지
-      env_plus_ret += proportion
-      syn += 1
-    elif(color == 3): #공장
-      env_minus_ret -= proportion * 1.25 / 2
-    elif(color == 7): #대형건물
+    if(color == 6): # 녹지
+      green_score += proportion
+      development_score -= proportion
+      green_synergy += 1
+    elif(color == 3): # 공장
+      factory_road_score -= proportion * 1.25 / 2
+    elif(color == 7): # 대형건물
       if(proportion <= 20):
-        conv_ret += proportion
+        convenience_score += proportion
       else:
-        conv_ret += 20
-    elif(color == 0): #도로
-      env_minus_ret -= proportion * 1.25 / 10
-    elif(color == 4): #강
-      env_plus_ret += proportion
-      syn += 1
-    elif(color == 1): #주택
-      hcount = count
-    elif(color == 2): #아파트
-      acount = count
-    elif(color == 5): #논밭
-      field = proportion
+        convenience_score += 20
+    elif(color == 0): # 도로
+      factory_road_score -= proportion * 1.25 / 10
+    elif(color == 4): # 강
+      green_score += proportion
+      development_score -= proportion
+      green_synergy += 1
+    elif(color == 1): # 주택
+      house_count = count
+    elif(color == 2): # 아파트
+      appartment_count = count
+    elif(color == 5): # 논밭
+      development_score -= proportion
 
-  if(syn == 2): #강 + 녹지 시너지
-    env_plus_ret += 5
+  if(green_synergy == 2): #강 + 녹지 시너지
+    green_score += 5
 
-  if acount + hcount != 0:
-    score_house = (100 * acount) / (acount+hcount)
+  if apartment_count + house_count != 0:
+    house_score = (100 * apartment_count) / (apartment_count + house_count)
   else:
-    score_house = 0
+    house_score = 0
   
-  response['standard'].append({"name" : "편의성 점수", "desc" : "대형건물 수", "score" : conv_ret})
-  response['standard'].append({"name" : "공장, 도로 점수", "desc" : "매연, 소음 등 마이너스 요소", "score" : env_minus_ret})
-  response['standard'].append({"name" : "녹지, 강 점수", "desc" : "환경 요소", "score" : env_plus_ret})
-  response['standard'].append({"name" : "주택/아파트 점수", "desc" : "개인 선호도", "score" : score_house})
-  response['standard'].append({"name" : "논밭 점수", "desc" : "개인 지표", "score" : field})
+  response['criterion'].append({"name" : "편의성 점수", "desc" : "주변의 대형 건물(관공서 등)의 비율", "score" : convenience_score})
+  response['criterion'].append({"name" : "공장, 도로 점수", "desc" : "매연과 소음을 발생시키는 공장과 도로의 비율", "score" : factory_road_score})
+  response['criterion'].append({"name" : "녹지, 강 점수", "desc" : "공기의 질과 정서적 안정에 도움을 주는 녹지와 수변공간의 비율", "score" : green_score})
+  response['criterion'].append({"name" : "주택/아파트 지수", "desc" : "구역에 주택과 아파트 중 어느 쪽이 많은지의 비율", "score" : house_score})
+  response['criterion'].append({"name" : "개발 지수", "desc" : "시내와 교외를 판단할 수 있는 개발도 점수", "score" : development_score})
   
-  response['score'] = env_plus_ret+env_minus_ret+conv_ret
+  response['score'] = green_score + factory_road_score + convenience_score
 
   return response
